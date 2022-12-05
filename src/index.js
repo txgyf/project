68bab3d9990a50f103941a3899d1574831d946dd
15110761762_39590 <15110761762@163.com>
2022-12-05 16:19:54 +0800

    初始化master

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./form -antd4/page/MyRcformPage";
+// import App from "./form -antd4/page/MyRcformPage";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
   document.getElementById("root")
 );
 
