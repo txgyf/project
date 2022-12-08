e826edeeddfad8a6d0c792650c131e003754ff9a
15110761762_39590 <15110761762@163.com>
2022-12-08 15:04:32 +0800

    router6初始化完成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,97 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  // HashRouter as Router,
+  // MemoryRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useNavigate,
+  useParams,
+  useResolvedPath,
+} from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+//   Outlet,
+//   useParams,
+//   useNavigate,
+//   useResolvedPath,
+// } from "./mini-react-router";
+
+export default function App(props) {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            {/* <Route path="product" element={<Product />} /> */}
+            <Route path="product" element={<Product />}>
+              <Route path=":id" element={<ProductDetail />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <Link to="/">首页</Link>
+      <Link to="product">商品</Link>
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Product() {
+  const path = useResolvedPath("123");
+  console.log("path", path); //sy-log
+  return (
+    <div>
+      <h1>Product</h1>
+      <Link to="123">详情</Link>
+      <Outlet />
+    </div>
+  );
+}
+
+function ProductDetail() {
+  const params = useParams();
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>ProductDetail: {params.id}</h1>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        go home
+      </button>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h1>NoMatch</h1>
+    </div>
+  );
+}
