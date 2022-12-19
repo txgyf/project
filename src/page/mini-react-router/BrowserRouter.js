a867420226a4f4e82623522ba3b0909dddce0587
15110761762_39590 <15110761762@163.com>
2022-12-19 10:10:03 +0800

    router提交

---
+++ src/page/mini-react-router/BrowserRouter.js
@@ -0,0 +1,3 @@
+export default function BrowserRouter() {
+  return <div>BrowserRouter</div>;
+}
---
+++ src/page/mini-react-router/index.js
@@ -0,0 +1,5 @@
+import BrowserRouter from "./BrowserRouter";
+import Route from "./Route";
+import Routes from "./Routes";
+
+export { BrowserRouter, Route, Routes };
